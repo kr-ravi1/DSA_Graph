2a1215d648835fd5cc1dadc1122f45427c7d2a64
Ravi Kumar <ravikr12082018@gmail.com>
2024-03-28 10:53:56 +0530

    using bfs in an undirected graph

--- Detect_cycle.java
+++ Detect_cycle.java
@@ -1,24 +1,43 @@
+
+
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
-        boolean[] vis = new boolean[V];
+        boolean vis[] = new boolean[V];
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                if(dfs(adj,vis,i,-1)) return true;
+                if(bfs(adj,vis,i)) return true;
             }
         }
         return false;
     }
     
-    public boolean dfs(ArrayList<ArrayList<Integer>> adj, boolean[] vis,int node,int parent){
+    public boolean bfs(ArrayList<ArrayList<Integer>> adj, boolean vis[], int node){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(node,-1));
         vis[node] = true;
-            for(int t : adj.get(node)){
-                if(!vis[t]){
-                    if(dfs(adj,vis,t,node)) return true;
+        while(!q.isEmpty()){
+            int currNode = q.peek().curr;
+            int parentNode = q.peek().parent;
+            q.remove();
+            for(int t : adj.get(currNode)){
+                if(!vis[t]) {
+                    q.add(new Pair(t,currNode));
+                    vis[t] = true;
                 }
-                else if(t != parent) return true;
+                else if(t != parentNode) return true; // cycle is present.
             }
+        }
         return false;
+    }
+}
+
+class Pair{
+    int curr;
+    int parent;
+    Pair(int curr, int parent){
+        this.curr = curr;
+        this.parent = parent;
     }
 }
