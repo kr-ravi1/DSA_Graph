a0502658b93f634e9ec1e98cca9e0d1f92b9acdd
Ravi Kumar <ravikr12082018@gmail.com>
2024-04-08 09:57:58 +0530

    Using DFS

--- Bipartite_graph.java
+++ Bipartite_graph.java
@@ -7,29 +7,23 @@ class Solution
         Arrays.fill(color,-1);
         for(int i=0;i<V;i++){
             if(color[i] == -1){
-                if(!bfs(V,adj,color,i)) return false;
+                if(!dfs(adj,color,i,0)) return false;
             }
         }
         return true;
     }
     
-    public boolean bfs(int V, ArrayList<ArrayList<Integer>>adj, int[] color, int node){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        color[node] = 0;
-        while(!q.isEmpty()){
-            int temp = q.peek();
-            q.remove();
-            for(int t : adj.get(temp)){
-                if(color[t] == -1){
-                    color[t] = 1 - color[temp];
-                    q.add(t);
-                }
-                else if(color[t] == color[temp]){
-                    return false;
-                }
+    public boolean dfs(ArrayList<ArrayList<Integer>> adj, int[] color, int node, int col){
+        color[node] = col;
+        
+        for(int t : adj.get(node)){
+            if(color[t] == -1){
+                if(!dfs(adj,color,t,1-col)) return false;
+            }
+            else if(color[t] == col){
+                return false;
             }
         }
         return true;
-    }
+    } 
 }
