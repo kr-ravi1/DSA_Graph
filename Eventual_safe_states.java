cd3df02b64085a4711b499ce5531f14848fe0424
Ravi Kumar <ravikr12082018@gmail.com>
2024-04-09 15:42:05 +0530

    Striver's Method

--- Eventual_safe_states.java
+++ Eventual_safe_states.java
@@ -1,27 +1,38 @@
 class Solution {
+
     List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
         // Your code here
         List<Integer> list = new ArrayList<>();
+        boolean[] vis = new boolean[V];
         boolean[] path = new boolean[V];
+        boolean[] check = new boolean[V];
         for(int i=0;i<V;i++){
-            boolean[] vis = new boolean[V];
-            if(dfs(adj,vis,path,i)) list.add(i);
+            if(!vis[i]){
+                dfs(adj,vis,path,check,i);
+            }
+        }
+        for(int i=0;i<V;i++){
+            if(check[i]){
+                list.add(i);
+            }
         }
         return list;
     }
     
-    boolean dfs(List<List<Integer>> adj, boolean[] vis, boolean[] path, int node){
+    boolean dfs(List<List<Integer>> adj, boolean[] vis, boolean[] path, boolean[] check, int node){
         vis[node] = true;
         path[node] = true;
+        check[node] = false;
         for(int t : adj.get(node)){
             if(!vis[t]) {
-                if(!dfs(adj,vis,path,t)) return false;
+                if(dfs(adj,vis,path,check,t)) return true;
             }
             else if(path[t]){
-                return false;
+                return true;
             }
         }
         path[node] = false;
-        return true;
+        check[node] = true;
+        return false;
     }
 }
