862b6a0f807ea31885a4740a76f85d5105ce4057
Ravi Kumar <ravikr12082018@gmail.com>
2024-04-11 19:31:08 +0530

    Khan's Algorithm

--- Detect_cycle.java
+++ Detect_cycle.java
@@ -1,43 +1,29 @@
-
-
 class Solution {
-    // Function to detect cycle in an undirected graph.
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        // Code here
-        boolean vis[] = new boolean[V];
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int[] inDegree = new int[V];
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(bfs(adj,vis,i)) return true;
+            for(int t : adj.get(i)){
+                inDegree[t]++;
             }
         }
-        return false;
-    }
-    
-    public boolean bfs(ArrayList<ArrayList<Integer>> adj, boolean vis[], int node){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(node,-1));
-        vis[node] = true;
+        
+        for(int i=0;i<V;i++){
+            if(inDegree[i] == 0) q.add(i);
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
         while(!q.isEmpty()){
-            int currNode = q.peek().curr;
-            int parentNode = q.peek().parent;
-            q.remove();
-            for(int t : adj.get(currNode)){
-                if(!vis[t]) {
-                    q.add(new Pair(t,currNode));
-                    vis[t] = true;
-                }
-                else if(t != parentNode) return true; // cycle is present.
+            int temp = q.poll();
+            list.add(temp);
+            for(int t : adj.get(temp)){
+                inDegree[t]--;
+                if(inDegree[t] == 0) q.add(t);
             }
         }
-        return false;
-    }
-}
-
-class Pair{
-    int curr;
-    int parent;
-    Pair(int curr, int parent){
-        this.curr = curr;
-        this.parent = parent;
+        
+        return list.size()!=V;
     }
 }
