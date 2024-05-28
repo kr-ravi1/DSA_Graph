580868bf42cb078fc5c6a0d7f46f38e50215cad4
Ravi Kumar <ravikr12082018@gmail.com>
2024-05-28 23:38:47 +0530

    Using topological sort Khan's Algo

--- Course_Schedule.java
+++ Course_Schedule.java
@@ -1,38 +1,38 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList());
         }
-
         int n = prerequisites.length;
         for(int i=0;i<n;i++){
             adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            inDegree[prerequisites[i][1]]++;
         }
-
-        boolean[] vis = new boolean[numCourses];
-        boolean[] pathVis = new boolean[numCourses];
-
+        
+        toposort(numCourses, adj, 0, inDegree);
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(isCycle(adj,vis,pathVis,i)) return false;
-            }
+            if(inDegree[i] > 0) return false;
         }
         return true;
     }
 
-    public boolean isCycle(List<List<Integer>> adj, boolean[] vis, boolean[] pathVis, int node){
-        vis[node] = true;
-        pathVis[node] = true;
-        for(int t : adj.get(node)){
-            if(!vis[t]){
-                if(isCycle(adj,vis,pathVis,t)) return true;
-            }
-            else if(pathVis[t]){
-                return true;
+    public void toposort(int n, List<List<Integer>> adj, int node, int[] inDegree){
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(inDegree[i] == 0){
+                q.add(i);
             }
         }
-        pathVis[node] = false;
-        return false;
+
+        while(!q.isEmpty()){
+            int temp = q.poll();
+            for(int t : adj.get(temp)){
+                inDegree[t]--;
+                if(inDegree[t] == 0) q.add(t);
+            }
+        }
+        return;
     }
 }
