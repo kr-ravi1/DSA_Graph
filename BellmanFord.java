08b093ebe0e62b9db0614af470c74e6783a97b09
Ravi Kumar <ravikr12082018@gmail.com>
2024-06-01 22:28:08 +0530

    organised code

--- BellmanFord.java
+++ BellmanFord.java
@@ -9,18 +9,24 @@ class Solution {
         dist[S] = 0;
         
         for(int i=0;i<V-1;i++){
-            for(int j=0;j<n;j++){
+            for(ArrayList<Integer> t : edges){
                 //relaxation
-                if(dist[edges.get(j).get(0)] != (int)(1e8) && dist[edges.get(j).get(0)] + edges.get(j).get(2) < dist[edges.get(j).get(1)]){
-                    dist[edges.get(j).get(1)] = dist[edges.get(j).get(0)] + edges.get(j).get(2);
+                int u = t.get(0);
+                int v = t.get(1);
+                int wt = t.get(2);
+                if(dist[u] != (int)(1e8) && dist[u] + wt < dist[v]){
+                    dist[v] = dist[u] + wt;
                 }
             }
         }
         
         // To check -ve cycle
-        for(int j=0;j<n;j++){
-            // Nth relaxation
-            if(dist[edges.get(j).get(0)] != (int)(1e8) && dist[edges.get(j).get(0)] + edges.get(j).get(2) < dist[edges.get(j).get(1)]){
+        for(ArrayList<Integer> t : edges){
+            //relaxation
+            int u = t.get(0);
+            int v = t.get(1);
+            int wt = t.get(2);
+            if(dist[u] != (int)(1e8) && dist[u] + wt < dist[v]){
                 int[] temp = new int[1];
                 temp[0] = -1;
                 return temp;
