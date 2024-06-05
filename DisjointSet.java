3950342ba352ce4bb0cd92ec0f7bc9425667b558
Ravi Kumar <ravikr12082018@gmail.com>
2024-06-05 07:10:33 +0530

    Union by size

--- DisjointSet.java
+++ DisjointSet.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class DisjointSetByUnion {
-    List<Integer> rank = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
     public DisjointSetByUnion(int n){
         for(int i=0;i<=n;i++){
-            rank.add(0);
             parent.add(i);
+            size.add(1);
         }
     }
 
@@ -18,20 +18,17 @@ class DisjointSetByUnion {
         return parent.get(node);
     }
 
-    public void unionByRank(int u, int v){
+    public void unionBySize(int u, int v){
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
         if(ulp_u == ulp_v) return;
-        if(rank.get(ulp_u) < rank.get(ulp_v)){
+        if(size.get(ulp_u) < size.get(ulp_v)) {
             parent.set(ulp_u, ulp_v);
-        }
-        else if(rank.get(ulp_v) < rank.get(ulp_u)){
-            parent.set(ulp_v, ulp_u);
+            size.set(ulp_v,size.get(ulp_v)+size.get(ulp_u));
         }
         else {
             parent.set(ulp_v, ulp_u);
-            int rankU = rank.get(ulp_u);
-            rank.set(ulp_u, rankU + 1);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
         }
     }
 }
@@ -39,11 +36,11 @@ class DisjointSetByUnion {
 public class DisjointSet {
     public static void main(String[] args){
         DisjointSetByUnion ds = new DisjointSetByUnion(7);
-        ds.unionByRank(1, 2);
-        ds.unionByRank(2, 3);
-        ds.unionByRank(4, 5);
-        ds.unionByRank(6, 7);
-        ds.unionByRank(5, 6);
+        ds.unionBySize(1, 2);
+        ds.unionBySize(2, 3);
+        ds.unionBySize(4, 5);
+        ds.unionBySize(6, 7);
+        ds.unionBySize(5, 6);
         if(ds.findUPar(3) == ds.findUPar(7)){
             System.out.println("Same Component");
         }
@@ -51,7 +48,7 @@ public class DisjointSet {
             System.out.println("Not same component");
         }
 
-        ds.unionByRank(3, 7);
+        ds.unionBySize(3, 7);
         if(ds.findUPar(3) == ds.findUPar(7)){
             System.out.println("Same Component");
         }
@@ -60,5 +57,3 @@ public class DisjointSet {
         }
     }
 }
-
-
